db84fd0913d33e8fcd71b5f6281bb3fd1b5e46be
iCloudboy <cohen.macdonald1@gmail.com>
2017-04-08 03:53:00 +0100

    num rows test

--- app/functions/login.func.php
+++ app/functions/login.func.php
@@ -15,104 +15,105 @@ if (isset($_POST['login'])) {
     $stmt->bind_param("s", $userIP);
     $stmt->execute();
 
+
     if ($stmt->num_rows() >= 3) {
         $_SESSION['login_error'] = "You have tried to log in too many times.";
         print_r($_SESSION['login_error']);
         header("Refresh:3; url=/login.php");
         exit();
     } else {
-        $stmt->close();
-        if (!empty($email) && !empty($password)) {
-            //if (filter_var($email, FILTER_VALIDATE_EMAIL)) { //check for valid email address
-            if (strlen($email) <= 50) { //check that the email address is no more than 50 characters
-                if (strlen($password) <= 50) {
-                    try {
-                        $stmt = $conn->prepare("SELECT userID, password FROM users WHERE email = ?");
-                        $stmt->bind_param("s", $email);
-                        $stmt->execute();
-                        $stmt->bind_result($userID, $passInDB);
-
-
-                        if ($stmt->fetch()) {
-                            if (password_verify($password, $passInDB)) {
-                                $stmt->close();
-                                $stmt2 = $conn->prepare("SELECT active FROM users WHERE userID = ?");
-                                $stmt2->bind_param("i", $userID);
-                                $stmt2->execute();
-                                $stmt2->bind_result($active);
-
-                                if ($stmt2->fetch()) {
-                                    if ($active === 0) {
-                                        $_SESSION['login_error'] = "User is not active. Please click the link in your activation email.";
-                                        //header("Refresh:3; url=/login.php");
-                                        print_r($_SESSION['login_error']);
-                                        exit();
-                                    } else {
-                                        $_SESSION['user'] = $userID;
-                                        header("Location: ../../index.php");
-                                    }
-                                } else {
-
-                                }
-                            } else {
-                                $stmt->close();
-                                $incorrectpasswordsql = "INSERT INTO ipaddress(loginIP) VALUES (?)";
-                                //$stmt3 = $conn->prepare($incorrectpasswordsql);
-
-                                if ($stmt3 = $conn->prepare($incorrectpasswordsql)){
-                                    $stmt3->bind_param("s", $userIP);
-                                    $stmt3->execute();
-                                }
-                                else {
-                                    die("Errormessage: ". $conn->error);
-                                }
-
-                                if ($stmt3 != false){
-                                    echo "incorrect password. Please try again.";
-                                    header("Refresh:3; url=/login.php");
-                                    exit();
-                                } else {
-                                    echo "Error storing ip address.";
-                                }
-
-
-                            }
-                        } else {
-
-
-                            $_SESSION['login_error'] = "Email address not registered. Please enter a valid email address";
-                            //header("Refresh:3; url=/login.php");
-                            print_r($_SESSION['login_error']);
-                            exit();
-                        }
-
-                    } catch (PDOException $e) {
-                        echo "Error: " . $e->getMessage();
-                    }
-                } else {
-                    $_SESSION['login_error'] = "Password must be no more than 50 characters. Please try again.";
-                    print_r($_SESSION['login_error']);
-                    header("Refresh:3; url=/login.php");
-                    exit();
-                }
-            } else {
-                $_SESSION['login_error'] = "Email address must be no more than 50 characters. Please try again.";
-                print_r($_SESSION['login_error']);
-                header("Refresh:3; url=/login.php");
-                exit();
-            }
-//        //} else {
-//            $_SESSION['login_error'] = "Please enter a valid email address.";
+        echo $stmt->num_rows();
+//        $stmt->close();
+//        if (!empty($email) && !empty($password)) {
+//            //if (filter_var($email, FILTER_VALIDATE_EMAIL)) { //check for valid email address
+//            if (strlen($email) <= 50) { //check that the email address is no more than 50 characters
+//                if (strlen($password) <= 50) {
+//                    try {
+//                        $stmt = $conn->prepare("SELECT userID, password FROM users WHERE email = ?");
+//                        $stmt->bind_param("s", $email);
+//                        $stmt->execute();
+//                        $stmt->bind_result($userID, $passInDB);
+//
+//
+//                        if ($stmt->fetch()) {
+//                            if (password_verify($password, $passInDB)) {
+//                                $stmt->close();
+//                                $stmt2 = $conn->prepare("SELECT active FROM users WHERE userID = ?");
+//                                $stmt2->bind_param("i", $userID);
+//                                $stmt2->execute();
+//                                $stmt2->bind_result($active);
+//
+//                                if ($stmt2->fetch()) {
+//                                    if ($active === 0) {
+//                                        $_SESSION['login_error'] = "User is not active. Please click the link in your activation email.";
+//                                        //header("Refresh:3; url=/login.php");
+//                                        print_r($_SESSION['login_error']);
+//                                        exit();
+//                                    } else {
+//                                        $_SESSION['user'] = $userID;
+//                                        header("Location: ../../index.php");
+//                                    }
+//                                } else {
+//
+//                                }
+//                            } else {
+//                                $stmt->close();
+//                                $incorrectpasswordsql = "INSERT INTO ipaddress(loginIP) VALUES (?)";
+//
+//                                if ($stmt3 = $conn->prepare($incorrectpasswordsql)){
+//                                    $stmt3->bind_param("s", $userIP);
+//                                    $stmt3->execute();
+//                                }
+//                                else {
+//                                    die("Errormessage: ". $conn->error);
+//                                }
+//
+//                                if ($stmt3 != false){
+//                                    echo "incorrect password. Please try again.";
+//                                    header("Refresh:3; url=/login.php");
+//                                    exit();
+//                                } else {
+//                                    echo "Error storing ip address.";
+//                                }
+//
+//
+//                            }
+//                        } else {
+//
+//
+//                            $_SESSION['login_error'] = "Email address not registered. Please enter a valid email address";
+//                            //header("Refresh:3; url=/login.php");
+//                            print_r($_SESSION['login_error']);
+//                            exit();
+//                        }
+//
+//                    } catch (PDOException $e) {
+//                        echo "Error: " . $e->getMessage();
+//                    }
+//                } else {
+//                    $_SESSION['login_error'] = "Password must be no more than 50 characters. Please try again.";
+//                    print_r($_SESSION['login_error']);
+//                    header("Refresh:3; url=/login.php");
+//                    exit();
+//                }
+//            } else {
+//                $_SESSION['login_error'] = "Email address must be no more than 50 characters. Please try again.";
+//                print_r($_SESSION['login_error']);
+//                header("Refresh:3; url=/login.php");
+//                exit();
+//            }
+////        //} else {
+////            $_SESSION['login_error'] = "Please enter a valid email address.";
+////            print_r($_SESSION['login_error']);
+////            header("Refresh:3; url=/login.php");
+////            exit();
+////        }
+//        } else {
+//            $_SESSION['login_error'] = "Input fields cannot be empty. Please try again.";
 //            print_r($_SESSION['login_error']);
 //            header("Refresh:3; url=/login.php");
 //            exit();
 //        }
-        } else {
-            $_SESSION['login_error'] = "Input fields cannot be empty. Please try again.";
-            print_r($_SESSION['login_error']);
-            header("Refresh:3; url=/login.php");
-            exit();
-        }
     }
 } else {
     $_SESSION['login_error'] = "An error has occurred. Please try again";
