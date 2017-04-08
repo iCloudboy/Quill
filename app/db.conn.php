c3fe6c601b0641abf498b0f645301927d785beda
iCloudboy <cohen.macdonald1@gmail.com>
2017-04-08 03:34:46 +0100

    changed dbo.conn.php

--- app/db.conn.php
+++ app/db.conn.php
@@ -4,4 +4,5 @@ $conn = mysqli_connect('localhost','quill','quill123','quill');
 if (mysqli_connect_errno()) {
     echo "<p>Connection failed:".mysqli_connect_error()."</p>\n";
     mysqli_close($conn);
-$conn->set_charset("utf8");
+}
+mysqli_set_charset($conn,"utf8");
