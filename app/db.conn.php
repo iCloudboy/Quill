cc062bc79bf4710742d8830400716219eee21a03
iCloudboy <cohen.macdonald1@gmail.com>
2017-04-10 14:47:26 +0100

    fixed the dropdown menu

--- app/db.conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost','root','root','individual');
-if (mysqli_connect_errno()) {
-    echo "<p>Connection failed:".mysqli_connect_error()."</p>\n";
-    mysqli_close($conn);
-}
-mysqli_set_charset($conn,"utf8");
