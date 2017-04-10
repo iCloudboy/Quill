95bd407677f3d6979e0cb5aa7637cfe7c2a9bb84
iCloudboy <cohen.macdonald1@gmail.com>
2017-04-10 14:45:33 +0100

    fixed the dropdown menu

--- resources/assets/js/app-min.js
+++ resources/assets/js/app-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".header-image").click(function(){1===$(this).attr("id")?($(".header-image-menu").show(),$(this).attr("id","0")):($(".header-image-menu").hide(),$(this).attr("id","1"))})});
+$(document).ready(function(){$(".header-image").click(function(){$(".header-image-menu").fadeToggle()})});
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,33 +6,6 @@ $(document).ready(function()
 {
     $(".header-image").click(function()
     {
-        var X=$(this).attr('id');
-        if(X===1)
-        {
-            $(".header-image-menu").show();
-            $(this).attr('id', '0');
-        }
-        else
-        {
-            $(".header-image-menu").hide();
-            $(this).attr('id', '1');
-        }
+        $(".header-image-menu").fadeToggle();
     });
-
-    // $(".header-image-menu").mouseup(function()
-    // {
-    //     return false;
-    // });
-    //
-    // $(".header-image").mouseup(function()
-    // {
-    //     return false;
-    // });
-    //
-    // $(document).mouseup(function()
-    // {
-    //     $(".header-image-menu").hide();
-    //     $(".header-image").attr('id', '');
-    // });
-
 });
