9cb47174b1d6c3427ae57d3802d8dd0105f23b59
iCloudboy <cohen.macdonald1@gmail.com>
2017-04-12 06:08:26 +0100

    Completed Image upload functionality

--- app/functions/uploadpicture.func.php
+++ app/functions/uploadpicture.func.php
@@ -2,7 +2,7 @@
 
 function change_profile_image($user_id, $file_temp, $file_extension){
     require "./app/db.conn.php";
-    $file_path = 'website/resources/images/picture/' . substr(md5(time()), 0, 10) . '.' . $file_extension; //generates a 10 character md5 hash from current time and uses it to name the picture.
+    $file_path = 'var/www/html/website/resources/images/picture/' . substr(md5(time()), 0, 10) . '.' . $file_extension; //generates a 10 character md5 hash from current time and uses it to name the picture.
     move_uploaded_file($file_temp, $file_path); //move the temporary file to the file path generated in $file_path
 
     try {
