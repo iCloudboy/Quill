ebe3a33b28ad6dac0ba27a30da927a28211e7260
iCloudboy <cohen.macdonald1@gmail.com>
2017-04-12 06:13:21 +0100

    Completed Image upload functionality

--- settings.php
+++ settings.php
@@ -41,7 +41,7 @@
                     }
 
                     if (empty(retrievePicture()) === false){
-                        echo '<img src="website/' . retrievePicture(). '" alt="' . retrieveForename() . '\'s profile picture">';
+                        echo '<img src="' . retrievePicture(). '" alt="' . retrieveForename() . '\'s profile picture">';
                     }
 
                     ?>
